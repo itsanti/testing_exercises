c87c201b322a4784ae3a0f0e7861d0301a56eada
Aleksandr <aleksandr.kurov@gmail.com>
2023-11-23 18:57:26 +0300

    Add test_compose_datetime_from

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,16 @@
+import datetime
+import pytest
+
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
 def test_compose_datetime_from():
-    pass
+    date = datetime.date.today()
+    dt = datetime.datetime(date.year, date.month, date.day, 22, 20)
+
+    assert compose_datetime_from('lalala', '22:20') ==  dt
+    assert compose_datetime_from('tomorrow', '22:20') == dt + datetime.timedelta(days=1)
+
+    with pytest.raises(ValueError):
+        compose_datetime_from('tomorrow', '22:AB')
+        compose_datetime_from('tomorrow', '1212')
