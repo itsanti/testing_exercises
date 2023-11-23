b07da8e08aabdd0c66fe4735a9d8b3f4da818b66
Aleksandr <aleksandr.kurov@gmail.com>
2023-11-23 20:03:29 +0300

    Add test_build_url

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,11 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    host_name = 'localhost'
+    relative_url = 'about'
+
+    assert build_url(host_name, relative_url) == f'{host_name}/{relative_url}'
+    assert build_url('', relative_url) == f'/{relative_url}'
+    assert build_url(host_name, '') == f'{host_name}/'
+    assert build_url(host_name, relative_url, {'a': 1}) == f'{host_name}/{relative_url}?a=1'
+    assert build_url(host_name, '', {'a': 1, 'b': 2}) == f'{host_name}/?a=1&b=2'
